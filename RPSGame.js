dafcb0c770363c10a6dbf65010c3537e6b7e9942
KrutikaMorande325 <86116173+KrutikaMorande325@users.noreply.github.com>
2024-04-02 20:19:29 +0530

    Update RPSGame.js

--- RPSGame.js
+++ RPSGame.js
@@ -7,10 +7,7 @@ const msg = document.querySelector("#msg");
 const userScorepara = document.querySelector("#user-score");
 const compScorepara = document.querySelector("#comp-score");
 
-//Here we have distributed our work in various functions and can be called various times and this form is called as MODULAR FORM where work work is distributed in the form of module
 
-
-//To generate random choice we use Math class with menthod called ramdom and we will multiply with such number so that we get index number one less than the multiplyed number for example we want index no upto 5 then we will multiply it with 6. but we will get decimal no so we will use a function called Math.floor()
 const genCompChoice = () => {
   const options = ["rock", "paper", "scissors"];
   const randIdx = Math.floor(Math.random() * 3);
@@ -43,9 +40,9 @@ const showWinner = (userWin, userChoice, compChoice) =>{
 const playGame = (userChoice) => {
   //Generate computer choice
 
-// console.log("User choice = ",userChoice);
+
        const compChoice = genCompChoice();
-    //    console.log("Comp choice = ",compChoice);
+  
 
   if (userChoice === compChoice) {
     //Draw Game
